92d0ffc26b27f0abad8956bd87658d54e364ea3d
Jonathan Johnston <johnstonj2026@ku.edu>
2026-04-18 01:07:45 -0500

    implemented move list

--- celebrity-love-island/src/game/index.js
+++ celebrity-love-island/src/game/index.js
@@ -2,6 +2,14 @@ export { ACTIONS, createGameReducer } from './reducer'
 export { createGameEngine, selectEliminatedContestant } from './engine'
 export { CAREER_OPTIONS, PLAYER_ID } from './data/contestants'
 export {
+  BATTLE_STATES,
+  PLAYER_MOVE_POOL,
+  STARTING_MOVE_IDS_BY_CAREER,
+  MOVE_POOL_BY_ID,
+  getMoveById,
+  getMovesForCareer,
+} from './data/moves'
+export {
   SEASON_LENGTH,
   isBombshellRound,
   isEliminationRound,
